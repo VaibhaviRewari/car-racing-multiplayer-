de9ad0da629555bea45dde23f090f464dd6ba6b6
VaibhaviRewari <78912700+VaibhaviRewari@users.noreply.github.com>
2021-05-22 19:23:22 +0530

    Add files via upload

---
+++ C38/asynchronousBallMovement/sketch.js
@@ -0,0 +1,33 @@
+var ball,position,database,form,player,game,allPlayers;
+var gameState=0;
+var playerCount=0;
+
+function setup(){
+    database=firebase.database();                
+    createCanvas(500,500);
+
+    game = new Game();
+    game.getState();
+    game.start();
+
+
+    
+}
+
+function draw(){
+background("white");
+
+    if(playerCount===4){
+    game.update(1);
+    }
+    if(gameState===1){
+    clear();    
+    game.play()
+;
+    }
+
+}
+
+
+
+
